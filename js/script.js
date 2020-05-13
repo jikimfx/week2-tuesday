bafc6ca11b5f7801f76088f1a4428e44754e647a
jikimfx <jikimfx@gmail.com>
2020-05-13 08:48:16 +0700

    Coin change rocket + checking for non-numbers

--- js/script.js
+++ js/script.js
@@ -50,32 +50,38 @@ const currencyTypes = {
 }
 
 let amountConvert = document.getElementById('amountConvert');
-let btn = document.getElementById("updateButton");
+//let btn = document.getElementById("updateButton");
 let active = 'EUR';
+
+let coinChangeAmount = document.getElementById('coinChangeAmount');
 
 updateButton.addEventListener("click", () => {
     let amt = amountConvert.value;
-    amt ? "" : alert("Please enter amount to convert");
-    let fromCurrency = document.querySelector('input[name="currencyRadioButton"]:checked').value
-    
-    hideSelectedFromResults(fromCurrency);
+    if ((amt == "") || (isNaN(amt) == true)) {
+        alert("Please enter amount to convert");
+    } else {
+        // amt ? ("" || (isNaN(amt) == true)) : alert("Please enter amount to convert");
+        let fromCurrency = document.querySelector('input[name="currencyRadioButton"]:checked').value
 
-    let toCurrency = ["EUR", "USD", "VND", "KRW", "JPY", "SGD"]
-    let result = [];
-    let displayResult = [];
-    let i;
-    for (i = 0; i < toCurrency.length; i++) {
-        (fromCurrency == toCurrency[i]) ? result[i] = amt : result[i] = (amt * currencyTypes[fromCurrency][toCurrency[i]]).toFixed(2);
-        // (from == to) ? result = amt : result = (amt * currencyTypes[from][to).toFixed(2);
-        displayResult[i] = showResult(toCurrency[i], result[i])
+        hideSelectedFromResults(fromCurrency);
+
+        let toCurrency = ["EUR", "USD", "VND", "KRW", "JPY", "SGD"]
+        let result = [];
+        let displayResult = [];
+        let i;
+        for (i = 0; i < toCurrency.length; i++) {
+            (fromCurrency == toCurrency[i]) ? result[i] = amt : result[i] = (amt * currencyTypes[fromCurrency][toCurrency[i]]).toFixed(2);
+            // (from == to) ? result = amt : result = (amt * currencyTypes[from][to).toFixed(2);
+            displayResult[i] = showResult(toCurrency[i], result[i])
+        }
+        // let notification = showResult(to, result);
+        document.getElementById("toEurResult").innerHTML = `${displayResult[0]}`;
+        document.getElementById("toUsdResult").innerHTML = `${displayResult[1]}`;
+        document.getElementById("toVndResult").innerHTML = `${displayResult[2]}`;
+        document.getElementById("toKrwResult").innerHTML = `${displayResult[3]}`;
+        document.getElementById("toJpyResult").innerHTML = `${displayResult[4]}`;
+        document.getElementById("toSgdResult").innerHTML = `${displayResult[5]}`;
     }
-    // let notification = showResult(to, result);
-    document.getElementById("toEurResult").innerHTML = `${displayResult[0]}`;
-    document.getElementById("toUsdResult").innerHTML = `${displayResult[1]}`;
-    document.getElementById("toVndResult").innerHTML = `${displayResult[2]}`;
-    document.getElementById("toKrwResult").innerHTML = `${displayResult[3]}`;
-    document.getElementById("toJpyResult").innerHTML = `${displayResult[4]}`;
-    document.getElementById("toSgdResult").innerHTML = `${displayResult[5]}`;
 })
 
 function formatCurrency(type, value) {
@@ -100,7 +106,7 @@ let showResult = (to, result) => {
 // Bootstrap d-flex has some trouble with adding a display:none, so have to remove the class altogether to hide
 
 function hideSelectedFromResults(active) {
-    if(active == 'EUR') {
+    if (active == 'EUR') {
         $('.eur').removeClass("d-flex").addClass('hide');
         $('.usd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
         $('.vnd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
@@ -109,7 +115,7 @@ function hideSelectedFromResults(active) {
         $('.sgd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
     }
 
-    if(active == 'USD') {
+    if (active == 'USD') {
         $('.usd').removeClass("d-flex").addClass('hide');
         $('.eur').removeClass("d-flex").addClass("d-flex").removeClass("hide");
         $('.vnd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
@@ -118,7 +124,7 @@ function hideSelectedFromResults(active) {
         $('.sgd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
     }
 
-    if(active == 'VND') {
+    if (active == 'VND') {
         $('.vnd').removeClass("d-flex").addClass('hide');
         $('.eur').removeClass("d-flex").addClass("d-flex").removeClass("hide");
         $('.usd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
@@ -127,7 +133,7 @@ function hideSelectedFromResults(active) {
         $('.sgd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
     }
 
-    if(active == 'KRW') {
+    if (active == 'KRW') {
         $('.krw').removeClass("d-flex").addClass('hide');
         $('.eur').removeClass("d-flex").addClass("d-flex").removeClass("hide");
         $('.usd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
@@ -136,7 +142,7 @@ function hideSelectedFromResults(active) {
         $('.sgd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
     }
 
-    if(active == 'JPY') {
+    if (active == 'JPY') {
         $('.jpy').removeClass("d-flex").addClass('hide');
         $('.eur').removeClass("d-flex").addClass("d-flex").removeClass("hide");
         $('.usd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
@@ -144,7 +150,7 @@ function hideSelectedFromResults(active) {
         $('.krw').removeClass("d-flex").addClass("d-flex").removeClass("hide");
         $('.sgd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
     }
-    if(active == 'SGD') {
+    if (active == 'SGD') {
         $('.sgd').removeClass("d-flex").addClass('hide');
         $('.eur').removeClass("d-flex").addClass("d-flex").removeClass("hide");
         $('.usd').removeClass("d-flex").addClass("d-flex").removeClass("hide");
@@ -153,5 +159,41 @@ function hideSelectedFromResults(active) {
         $('.jpy').removeClass("d-flex").addClass("d-flex").removeClass("hide");
     }
 }
+
+coinChangeButton.addEventListener("click", () => {
+    let amt = Math.round(coinChangeAmount.value / 1000) * 1000;
+    if ((amt == "") || (isNaN(amt) == true)) {
+        alert("Please enter amount to change into smaller bills");
+    } else {
+        let fiveHundredK = parseInt(amt / 500000);
+        amt = amt % 500000;
+        let twoHundredK = parseInt(amt / 200000);
+        amt = amt % 200000;
+        let oneHundredK = parseInt(amt / 100000);
+        amt = amt % 100000;
+        let fiftyK = parseInt(amt / 50000);
+        amt = amt % 50000;
+        let twentyK = parseInt(amt / 20000);
+        amt = amt % 20000;
+        let tenK = parseInt(amt / 10000);
+        amt = amt % 10000;
+        let fiveK = parseInt(amt / 5000);
+        amt = amt % 5000;
+        let twoK = parseInt(amt / 2000);
+        amt = amt % 2000;
+        let oneK = parseInt(amt / 1000);
+        amt = amt % 1000;
+
+        document.getElementById("coinChangeResult-500").innerHTML = `${fiveHundredK}`;
+        document.getElementById("coinChangeResult-200").innerHTML = `${twoHundredK}`;
+        document.getElementById("coinChangeResult-100").innerHTML = `${oneHundredK}`;
+        document.getElementById("coinChangeResult-50").innerHTML = `${fiftyK}`;
+        document.getElementById("coinChangeResult-20").innerHTML = `${twentyK}`;
+        document.getElementById("coinChangeResult-10").innerHTML = `${tenK}`;
+        document.getElementById("coinChangeResult-5").innerHTML = `${fiveK}`;
+        document.getElementById("coinChangeResult-2").innerHTML = `${twoK}`;
+        document.getElementById("coinChangeResult-1").innerHTML = `${oneK}`;
+    }
+})
 
 hideSelectedFromResults(active);
